8180a82f90f294ac7ed7c552bf8ff5a929775058
hd2g <>
2026-01-06 00:52:04 +0900

    [Main] cli

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,21 @@ module Main (main) where
 
 import Prelude
 
-import qualified MyLib (someFunc)
+import Data.Foldable (for_)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+
+import Kaprekar
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  args <- getArgs
+  case args of
+    [x] ->
+      case readIntMaybe x of
+        Just n -> flip for_ print $ take n kaprekars
+        Nothing -> putStrLn $ "Invalid nat: " ++ x
+    _ -> putStrLn $ "***Invalid argument***"
+
+readIntMaybe :: String -> Maybe Int
+readIntMaybe = readMaybe
